c1768c063c8115965efabda08a20f73926500478
JustGreg <64327566+JustGregGithub@users.noreply.github.com>
2023-08-06 02:29:00 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 # Perform a Git pull to update the code
 git pull
 
+npm run build
+
 # Change ownership of storage directory
 sudo chown -R www-data:www-data storage
 
 # Change ownership of bootstrap/cache directory
 sudo chown -R www-data:www-data bootstrap/cache
+
+echo "Done!"
