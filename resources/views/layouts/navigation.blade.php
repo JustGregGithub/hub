0154d0ba4e5c527c78843eca6fc8b4114a3a2e94
JustGreg <64327566+JustGregGithub@users.noreply.github.com>
2024-02-11 14:55:17 +0000

    Change logo path

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,7 +9,7 @@
 
 <aside id="separator-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-neutral-900">
-        <img src="/images/logo.webp" alt="Rockford Logo" class="h-32 m-auto">
+        <img src="{{ asset('images/logo.png') }}" alt="Rockford Logo" class="h-32 m-auto">
         <hr class="h-px my-8 mt-2 mb-2 bg-gray-200 border-0 dark:bg-gray-700 ">
         <ul class="space-y-2 font-medium">
             <li>
